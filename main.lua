80f1c1f8937e23317e982266a687c47a4ba5f746
Trench Rat <112545354+VoidNmbrZero@users.noreply.github.com>
2024-11-22 17:52:14 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,7 @@ local hum = char.Humanoid
 local force = Instance.new("RocketPropulsion") force.Parent = char.Torso
 
 force.Target = workspace.whiplash3r.Head
+force.CartoonFactor = 100
+force.MaxThrust = 100
+force.MaxSpeed = 100
 force:Fire()
