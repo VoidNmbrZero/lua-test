b22cfdd7647835c5639e9853d3b6bdd64dfde2ab
Trench Rat <112545354+VoidNmbrZero@users.noreply.github.com>
2024-11-22 18:23:41 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 local lp = game.Players.LocalPlayer
 local char = lp.Character
 local hum = char.Humanoid
-local a0 = Instance.new("Attachment") a0.Parent = workspace.whiplash3r.Torso
-local a1 = Instance.new("Attachment") a1.Parent = char.Torso
-local force = Instance.new("LineForce") force.Parent = char.Torso
 
-
-force.Attachment0 = a0
-force.Attachment1 = a1
-force.Enabled = true
+for _, v in next, char:getDescendants() do
+  if v:isA("BasePart") and v.Name ~= "HumanoidRootPart" then
+    game["Run Service"].Heartbeat:Connect(function()
+      v.Velocity = Vector3.new(-30, 0, 0)
+    end
+  end
+end
