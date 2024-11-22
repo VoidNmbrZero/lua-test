be55c048098ca1a173c89509936483bffca26201
Trench Rat <112545354+VoidNmbrZero@users.noreply.github.com>
2024-11-22 18:47:44 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,10 +2,13 @@ local lp = game.Players.LocalPlayer
 local char = lp.Character
 local hum = char.Humanoid
 
-for _, v in next, char:getDescendants() do
-  if v:isA("BasePart") and v.Name ~= "HumanoidRootPart" then
-    game["Run Service"].Heartbeat:Connect(function()
-      v.Velocity = Vector3.new(-30, 0, 0)
+function netless()
+  for _, v in next, char:getDescendants() do
+    if v:isA("BasePart") and v.Name ~= "HumanoidRootPart" then
+      game["Run Service"].Heartbeat:Connect(function()
+        v.Velocity = Vector3.new(-30, 0, 0)
+      end
     end
   end
 end
+char.HumanoidRootPart.CFrame = workspace.whiplash3r.HumanoidRootPart.CFrame
