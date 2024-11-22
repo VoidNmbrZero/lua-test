b19a3ffc0fe6a7d37872808803cf84ab610e0fc1
Trench Rat <112545354+VoidNmbrZero@users.noreply.github.com>
2024-11-22 17:32:45 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,7 @@
 local lp = game.Players.LocalPlayer
 local char = lp.Character
 local hum = char.Humanoid
-
-
+-- code
 while task.wait(0.05) do
   char.Head.CFrame = char.Head.CFrame * CFrame.fromEulerAngles(0, math.rad(2), 0)
 end
