48874f47a30a1502654f560284dd13473a678485
Trench Rat <112545354+VoidNmbrZero@users.noreply.github.com>
2024-11-22 17:22:55 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 local lp = game.Players.LocalPlayer
 
 
-while task.wait(0.05) do
-  lp.Character.Head.CFrame.Angles = lp.Character.Head.CFrame.Angles + CFrame.Angles(0, 2, 0)
-end
+lp.Character.Head.CFrame = CFrame.new(0, 0, 0)
